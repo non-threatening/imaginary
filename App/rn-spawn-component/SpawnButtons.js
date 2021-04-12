3828ab64f5bf3e0e00be5fd410885e2082a59de9
non-threatening <imaginary.theremin@gmail.com>
2021-04-12 18:45:37 +0200

    rn-spawn-component

---
+++ App/rn-spawn-component/SpawnButtons.js
@@ -0,0 +1,145 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {useSpawnArray} from './spawnContext';
+
+export const AddSpawn = props => {
+  const [{}, dispatch] = useSpawnArray();
+  const add = () => {
+    dispatch({
+      type: 'NEW_SPAWN',
+      name: props.name,
+    });
+  };
+  return (
+    <Button
+      onPress={() => add()}
+      label={props.label ? props.label : 'Add'}
+      prepend={props.prepend ? props.prepend : null}
+      append={props.append ? props.append : null}
+      textStyle={props.textStyle ? props.textStyle : null}
+      style={props.style ? props.style : null}
+      buttonColor={props.buttonColor ? props.buttonColor : null}
+      pressedColor={props.pressedColor ? props.pressedColor : null}
+      android_ripple={props.android_ripple ? props.android_ripple : null}
+    />
+  );
+};
+
+export const RemoveSpawn = props => {
+  const [{}, dispatch] = useSpawnArray();
+  const remove = () => {
+    dispatch({
+      type: 'REMOVE_SINGLE_SPAWN',
+      payload: props.spawn,
+    });
+  };
+  return (
+    <Button
+      onPress={() => remove(props.spawn)}
+      label={props.label ? props.label : 'X'}
+      prepend={props.prepend ? props.prepend : null}
+      append={props.append ? props.append : null}
+      textStyle={props.textStyle ? props.textStyle : null}
+      style={props.style ? props.style : null}
+      buttonColor={props.buttonColor ? props.buttonColor : null}
+      pressedColor={props.pressedColor ? props.pressedColor : null}
+      android_ripple={props.android_ripple ? props.android_ripple : null}
+    />
+  );
+};
+
+export const ClearSpawns = props => {
+  const [{}, dispatch] = useSpawnArray();
+  const removeKind = () => {
+    dispatch({
+      type: 'REMOVE_NAME',
+      name: props.name,
+    });
+  };
+  return (
+    <Button
+      onPress={() => removeKind()}
+      label={props.label ? props.label : 'Clear by name'}
+      prepend={props.prepend ? props.prepend : null}
+      append={props.append ? props.append : null}
+      textStyle={props.textStyle ? props.textStyle : null}
+      style={props.style ? props.style : null}
+      buttonColor={props.buttonColor ? props.buttonColor : null}
+      pressedColor={props.pressedColor ? props.pressedColor : null}
+      android_ripple={props.android_ripple ? props.android_ripple : null}
+    />
+  );
+};
+
+export const RemoveAll = props => {
+  const [{}, dispatch] = useSpawnArray();
+  const removeAll = () => {
+    dispatch({
+      type: 'KILL_ALL_SPAWN',
+    });
+  };
+  return (
+    <>
+      <Button
+        onPress={() => removeAll()}
+        label={props.label ? props.label : 'Clear all Spawned'}
+        prepend={props.prepend ? props.prepend : null}
+        append={props.append ? props.append : null}
+        textStyle={props.textStyle ? props.textStyle : null}
+        style={props.style ? props.style : null}
+        buttonColor={props.buttonColor ? props.buttonColor : null}
+        pressedColor={props.pressedColor ? props.pressedColor : null}
+        android_ripple={props.android_ripple ? props.android_ripple : null}
+      />
+    </>
+  );
+};
+
+function Button({
+  android_ripple,
+  append,
+  buttonColor,
+  label,
+  onPress,
+  prepend,
+  pressedColor,
+  style,
+  textStyle,
+}) {
+  return (
+    <Pressable
+      onPress={onPress}
+      android_ripple={android_ripple}
+      style={({pressed}) => [
+        styles.button,
+        {
+          backgroundColor: pressed
+            ? pressedColor
+              ? pressedColor
+              : 'rgb(210, 230, 255)'
+            : buttonColor
+            ? buttonColor
+            : 'rgba(255, 255, 255, 0.2)',
+        },
+        style,
+      ]}>
+      <View style={{flexDirection: 'row', alignItems: 'center'}}>
+        <View>{prepend ? prepend : null}</View>
+        <Text style={[textStyle, {}]}>{label}</Text>
+        <View>{append ? append : null}</View>
+      </View>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    padding: 10,
+    margin: 10,
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+  },
+});
---
+++ App/rn-spawn-component/spawnContext.js
@@ -0,0 +1,62 @@
+import React, {createContext, useContext, useReducer} from 'react';
+
+export const initialState = {
+  spawnArray: [],
+};
+
+export const reducer = (state, action) => {
+  const newSpawn = name => {
+    let incSpawn = Number.isFinite(
+      Math.max(...state.spawnArray.map(thing => thing[0])),
+    )
+      ? Math.max(...state.spawnArray.map(thing => thing[0])) + 1
+      : 0;
+    state.spawnArray.push([incSpawn, name]);
+    const arraySpawn = [...state.spawnArray];
+    return state.spawnArray.length ? arraySpawn : [0, name];
+  };
+
+  const removeSpawn = removed => {
+    const newArr = [...state.spawnArray];
+    const del = [...newArr.map(thing => thing[0])];
+    removed = del.indexOf(removed);
+    newArr.splice(removed, 1);
+    return newArr.length ? newArr : [];
+  };
+
+  const removeKind = name => {
+    const swSpawn = state.spawnArray.filter(thing => !thing.includes(name));
+    return swSpawn;
+  };
+
+  switch (action.type) {
+    case 'KILL_ALL_SPAWN':
+      return {
+        spawnArray: [],
+      };
+    case 'NEW_SPAWN':
+      return {
+        spawnArray: newSpawn(action.name),
+      };
+    case 'REMOVE_SINGLE_SPAWN':
+      return {
+        spawnArray: removeSpawn(action.payload),
+      };
+    case 'REMOVE_NAME':
+      return {
+        spawnArray: removeKind(action.name),
+      };
+    default:
+      return state;
+  }
+};
+
+export const SpawnStateContext = createContext();
+
+export const SpawnProvider = ({children}) => (
+  <SpawnStateContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </SpawnStateContext.Provider>
+);
+
+export const useSpawnArray = () => useContext(SpawnStateContext);
