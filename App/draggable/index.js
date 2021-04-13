cb43c4ded9d6a453aac3cd3fe8b2a5cb36b25f4f
non-threatening <imaginary.theremin@gmail.com>
2021-04-14 01:20:33 +0200

    style

--- App/draggable/index.js
+++ App/draggable/index.js
@@ -1,14 +1,23 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {DraggableBox} from './Draggable';
+import {height, width} from '../style';
 
 export const KnobWrap = props => {
   const name = props.name;
   const spawnNum = props.spawnNumber;
+
   return (
-    <View style={{width: 50}}>
+    <View style={styles.box}>
       <DraggableBox spawnNum={spawnNum} name={name} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  box: {
+    left: width * 0.5 - 30,
+    position: 'absolute',
+    top: height * 0.5 - 70,
+  },
+});
